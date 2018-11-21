733db8dd582d27406c8c3f93b7498db94e95d06d
Gabriel <gabrieltlopessjc@hotmail.com>
2018-11-20 23:53:23 -0200

    Finished basic tests

---
+++ doc/SmokeTest.java
@@ -0,0 +1,5 @@
+/*
+Tentar logar no sistema com todos os dados que foram cadastrados no sistema corretamente, caso o acesso seja negado,
+significa que há um erro ou na criação do usuário, ou na validação, ou em ambos, impossibilitando a execução dos demais
+testes do programa
+*/
